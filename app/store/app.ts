66241f0ed20d5b0cc913527b69067f1c6da35c9f
wistoch <wistoch@qq.com>
2023-06-01 14:26:55 +0800

    modify config tightBorder

--- app/store/app.ts
+++ app/store/app.ts
@@ -139,7 +139,7 @@ const DEFAULT_CONFIG: ChatConfig = {
   avatar: "1f603",
   fontSize: 14,
   theme: Theme.Auto as Theme,
-  tightBorder: false,
+  tightBorder: true,
   sendPreviewBubble: false,
 
   disablePromptHint: false,
